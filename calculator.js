750c696dc9055b17ca6d4cadc6eaca6da18f6df6
RobertBostrom4 <Robertbostrom4@gmail.com>
2022-11-23 01:19:41 -0700

    Add operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,18 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return parseFloat(a / b);
 }
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
+console.log(operate("/", 1, 2));
